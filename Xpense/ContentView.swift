737844786055d2c280867dbd927431c95af0c3c6
Teddy Santya <teddy.santya@gmail.com>
2020-05-08 01:22:04 +0700

    Initial Commit

---
+++ Xpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xpense
+//
+//  Created by Teddy Santya on 8/5/20.
+//  Copyright © 2020 Teddy Santya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
